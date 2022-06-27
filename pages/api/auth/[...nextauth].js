b22701916b9e13398e35b2572484ce53cc20c424
ChintanJoshi14 <chintan15joshi@gmail.com>
2022-06-27 16:03:05 +0530

    Changed clientSecret to secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,7 +6,7 @@ export default NextAuth({
     providers: [
         TwitterProvider({
             clientId: process.env.TWITTER_CLIENT_ID,
-            clientSecret: process.env.TWITTER_CLIENT_SECRET,
+            secret: process.env.TWITTER_CLIENT_SECRET,
             version: "2.0",
         }),
     ],
